e532c1a81fdcc8edae7d3e9eeaef8b43d1c2ea4f
adrianMoyaMartin <125206876+adrianMoyaMartin@users.noreply.github.com>
2024-06-07 11:28:38 +0200

    added cpu saving mode after 5s of inactivity

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,21 @@ mod utils {
 use crate::utils::mods;
 
 use std::thread::sleep;
+use std::time::Instant;
 use std::time::Duration;
 
 fn main() {
     let mut last_state: [bool; 255] = [false; 255];
 
     let (audio_data, _stream, stream_handle) = mods::create_variables();
+    let mut last_touched = Instant::now();
 
     loop {
         for i in 0..255 {
             let state = unsafe { winuser::GetAsyncKeyState(i) };
 
             if (state & (0x8000u16 as i16)) != 0 {
+                last_touched = Instant::now();
                 if last_state[i as usize] == false && i != 1 && i != 2 && i != 3 {
                     mods::play_sound(&audio_data, &_stream, &stream_handle, i as usize);
                 }
@@ -28,7 +31,10 @@ fn main() {
                 last_state[i as usize] = false;
             }
         }
-
-        sleep(Duration::from_millis(5));
+        if last_touched.elapsed().as_millis() >= 5000 {
+            sleep(Duration::from_millis(50));
+        } else {
+            sleep(Duration::from_millis(5));
+        }
     }
 }
